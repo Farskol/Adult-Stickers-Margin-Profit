77c9b4e221fcd1e89c9efa8c8169417f45cbade5
Работа <hosnateymori85741@gmail.com>
2023-04-19 20:08:15 +0300

    commit

--- tgBot.js
+++ tgBot.js
@@ -3,9 +3,9 @@ const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot('5997079179:AAGGzi65hhsZv6LRhJ_q3Yk5OKZXSOuOcNw', {
     polling: true
 });
-bot.onText(/привет/, async (msg) => {
-    console.log(msg)
-})
+// bot.onText(/привет/, async (msg) => {
+//     console.log(msg)
+// })
 module.exports.sendMessage = async function(message) {
     await bot.sendMessage(-1001795301692,message);
 }
