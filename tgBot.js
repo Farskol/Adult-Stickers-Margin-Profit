79c376df0a008a7c8c21febc64113cda804c24e0
Работа <hosnateymori85741@gmail.com>
2023-04-20 19:18:13 +0300

    commit

--- tgBot.js
+++ tgBot.js
@@ -14,10 +14,10 @@ const bot = new TelegramBot('5997079179:AAGGzi65hhsZv6LRhJ_q3Yk5OKZXSOuOcNw', {
 //     ind.getInfo(today);
 // })
 module.exports.sendMessage = async function(message) {
-    //await bot.sendMessage(-1001795301692,message);
-    await bot.sendMessage(5752647120,message);
+    await bot.sendMessage(-1001795301692,message);
+    //await bot.sendMessage(5752647120,message);
 }
 module.exports.sendDocument = async function(link) {
-    //await bot.sendDocument(-1001795301692, link)
-    await bot.sendDocument(5752647120, link)
+    await bot.sendDocument(-1001795301692, link)
+    //await bot.sendDocument(5752647120, link)
 }
