9883fe0396521637587d7c3ba2b837453aac57de
Работа <hosnateymori85741@gmail.com>
2023-04-21 11:57:08 +0300

    commit

--- tgBot.js
+++ tgBot.js
@@ -4,15 +4,15 @@ const ind = require("./index");
 const bot = new TelegramBot('5997079179:AAGGzi65hhsZv6LRhJ_q3Yk5OKZXSOuOcNw', {
     polling: true
 });
-// bot.onText(/start/, async (msg) => {
-//     console.log(msg)
-//     let today = new Date();
-//     let day = String(today.getDate()).padStart(2,'0') - 1;
-//     let month = String(today.getMonth()+1).padStart(2,'0');
-//     let year = today.getFullYear();
-//     today = day + '.' + month + '.' + year;
-//     ind.getInfo(today);
-// })
+bot.onText(/start/, async (msg) => {
+    console.log(msg)
+    // let today = new Date();
+    // let day = String(today.getDate()).padStart(2,'0') - 1;
+    // let month = String(today.getMonth()+1).padStart(2,'0');
+    // let year = today.getFullYear();
+    // today = day + '.' + month + '.' + year;
+    // ind.getInfo(today);
+})
 module.exports.sendMessage = async function(message) {
     await bot.sendMessage(-1001795301692,message);
     //await bot.sendMessage(5752647120,message);
